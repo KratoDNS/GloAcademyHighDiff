299fa94ab8a888a1acd307eb677432e124140ab7
KratoDNS <gena.systerov@yandex.ru>
2020-10-12 20:33:54 +0300

    удалил точку с запятой и добавил slice

--- script/script.js
+++ script/script.js
@@ -1,11 +1,17 @@
 let num=266219
 let str= String(num)
-console.log(str)
-console.log(str.split('',6))
+
+
 let calc=1
  for ( let i = 0; i < str.length; i++){
-     console.log(calc *=str[i])}; 
-console.log(calc**3)
+     console.log(calc *=str[i])
+ }
 let final=String(calc**3)
+
+
+console.log(str)
+console.log(str.split('',6))
 console.log(final)
 console.log(final[0],final[1])
+console.log(calc**3)
+console.log('' + final.slice(0, 2))
